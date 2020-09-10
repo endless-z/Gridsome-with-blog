34fcb84fa21fbf4d4b8d068263acf3443d3fe086
yjx <578755236@qq.com>
2020-09-10 20:57:15 +0800

    文章标签

--- gridsome.config.js
+++ gridsome.config.js
@@ -19,7 +19,7 @@ module.exports = {
       options: {
         apiURL: 'http://localhost:1337',
         queryLimit: 1000, // Defaults to 100
-        contentTypes: ['post']
+        contentTypes: ['post', 'tag']
         // Possibility to login with a Strapi user,
         // when content types are not publicly available (optional).
         // loginData: {
@@ -28,5 +28,19 @@ module.exports = {
         // }
       }
     }
-  ]
+  ],
+  templates: {
+    StrapiPost: [
+      {
+        path: '/post/:id',
+        component: './src/templates/Post.vue'
+      }
+    ],
+    StrapiTag: [
+      {
+        path: '/tag/:id',
+        component: './src/templates/Tag.vue'
+      }
+    ]
+  }
 }
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -19,19 +19,27 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
-          <div class="post-preview">
-            <a href="post.html">
-              <h2 class="post-title">Man must explore, and this is exploration at its greatest</h2>
-              <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3>
-            </a>
+          <div class="post-preview" v-for="edge in $page.posts.edges" :key="edge.node.id">
+            <g-link :to="'/post/' + edge.node.id">
+              <h2 class="post-title">{{ edge.node.title }}</h2>
+              <!-- <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3> -->
+            </g-link>
             <p class="post-meta">
               Posted by
-              <a href="#">Start Bootstrap</a>
-              on September 24, 2019
+              <g-link
+                href="#"
+              >{{ edge.node.created_by.firstname + edge.node.created_by.lastname }}</g-link>
+              on {{ edge.node.created_at }}
             </p>
+            <p>
+              <span v-for="tag in edge.node.tags" :key="tag.id">
+                <g-link :to="'/tag/' + tag.id">{{ tag.title }}</g-link>
+                &nbsp;&nbsp;
+              </span>
+            </p>
+            <hr />
           </div>
-          <hr />
-          <div class="post-preview">
+          <!-- <div class="post-preview">
             <a href="post.html">
               <h2
                 class="post-title"
@@ -70,23 +78,56 @@
               <a href="#">Start Bootstrap</a>
               on July 8, 2019
             </p>
-          </div>
+          </div>-->
           <hr />
           <!-- Pager -->
-          <div class="clearfix">
+          <!-- <div class="clearfix">
             <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
-          </div>
+          </div> -->
+          <pager :info="$page.posts.pageInfo"></pager>
         </div>
       </div>
     </div>
   </Layout>
 </template>
 
+<page-query>
+query ($page: Int) {
+  posts: allStrapiPost (perPage: 1, page: $page) @paginate {
+    pageInfo {
+      totalPages
+      currentPage
+    }
+    edges {
+      node {
+        id
+        title
+        created_by {
+          id
+          firstname
+          lastname
+        }
+        tags {
+          id
+          title
+        }
+        created_at
+      }
+    }
+  }
+}
+</page-query>
+
 <script>
+import { Pager } from 'gridsome'
 export default {
   metaInfo: {
     title: "Hello, world!",
   },
+  name: 'HomePage',
+  components: {
+    Pager
+  }
 };
 </script>
 
